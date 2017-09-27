52d443ed7ea6357a469ed88e21645f79111325c0
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-09-27 00:21:29 -0300

    Ejjjjjjjjjjjjjjjjjjjjje

--- Scripts/Empresa_CREAR_TABLA.sql
+++ Scripts/Empresa_CREAR_TABLA.sql
@@ -4,17 +4,17 @@ CREATE TABLE Empresa (
 	Cuit NVARCHAR(50) NOT NULL UNIQUE,
 	Domicilio NVARCHAR(255) NOT NULL,
 	Habilitada BIT,
-	Rubro INT FOREIGN KEY REFERENCES Rubro(ID)
+	Id_Rubro INT FOREIGN KEY REFERENCES Rubro(ID)
 );
 
-INSERT INTO Empresa (Nombre, Cuit, Domicilio, Habilitada, Rubro)
+INSERT INTO Empresa (Nombre, Cuit, Domicilio, Habilitada, Id_Rubro)
 
 	SELECT DISTINCT 
 
 		[Empresa_Nombre] AS Nombre,
 		[Empresa_Cuit] AS Cuit,
 		[Empresa_Direccion] AS Domicilio,
-		Habilitado = 1,
-		[Empresa_Rubro] AS Rubro
+		Habilitada = 1,
+		[Empresa_Rubro] AS Id_Rubro
 
 	FROM [GD2C2017].[gd_esquema].[Maestra];
