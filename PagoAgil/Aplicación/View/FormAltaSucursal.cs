81c4e2484724aad2051e3019557046cf399f6594
Erik-1012 <bernaderi@hotmail.com>
2017-10-17 01:15:23 -0300

    1er aprox ABM Sucursal

---
+++ PagoAgil/Aplicación/View/FormAltaSucursal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgil.Aplicación.View
+{
+    public partial class FormAltaSucursal : Form
+    {
+        public FormAltaSucursal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
