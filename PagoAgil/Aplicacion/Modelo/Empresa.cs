151527f0571e5beda8d99fec1a1fb14851fafe6c
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-10-18 03:29:52 -0300

    Ultra gilada

---
+++ PagoAgil/Aplicacion/Modelo/Empresa.cs
@@ -0,0 +1,63 @@
+﻿using PagoAgil.Aplicacion.Modelo;
+using System;
+
+namespace PagoAgil.Aplicacion.Modelo
+{
+    public class Empresa : Identificable
+    {
+
+        private string cuit;
+        private string direccion;
+        private Rubro rubro;
+        private ushort diaRendicion;
+
+        Empresa (long unId, string unNombre, bool unEstado, string unCuit, string unaDireccion, Rubro unRubro, ushort unDia) : base (unId, unNombre, unEstado) {
+            this.Cuit = unCuit;
+            this.Direccion = unaDireccion;
+            this.RubroDeEmpresa = unRubro;
+            this.DiaRendicion = unDia;
+        }
+
+        public string Cuit
+        {
+            get { return cuit; }
+            set { cuit = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public Rubro RubroDeEmpresa
+        {
+            get { return rubro; }
+            set { rubro = value; }
+        }
+
+        public ushort DiaRendicion
+        {
+            get { return diaRendicion; }
+            set { diaRendicion = value; }
+        }
+
+        override public void deshabilitar()
+        {
+            this.Estado = this.facturasRendidas();
+        }
+
+        private bool facturasRendidas()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void rendir()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+
+}
---
+++ PagoAgil/Aplicacion/Modelo/Identificable.cs
@@ -0,0 +1,43 @@
+﻿namespace PagoAgil.Aplicacion.Modelo
+{
+    public abstract class Identificable
+    {
+        private long id;
+        private string nombre;
+        private bool habilitada;
+
+        public Identificable (long unId, string unNombre, bool unEstado)
+        {
+            id = unId;
+            this.Nombre = unNombre;
+            this.Estado = unEstado;
+        }
+
+        public long Id
+        {
+            get { return id; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public bool Estado
+        {
+            get { return habilitada; }
+            set { habilitada = value; }
+        }
+
+        public virtual void habilitar()
+        {
+            this.Estado = true;
+        }
+
+        public virtual void deshabilitar()
+        {
+            this.Estado = false;
+        }
+    }
+}
