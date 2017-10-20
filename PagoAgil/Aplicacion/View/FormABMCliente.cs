55f555142ebb1baef21c76e7eb95750fdcec8728
nrmerlis <nicoxeneize.10@gmail.com>
2017-10-20 12:58:27 -0300

    Se agrega ABM cliente

---
+++ PagoAgil/Aplicacion/View/FormABMCliente.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgil.Aplicacion.View
+{
+    class FormABMCliente : Form
+    {
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn nombre;
+        private DataGridViewTextBoxColumn apellido;
+        private DataGridViewTextBoxColumn dni;
+        private DataGridViewTextBoxColumn mail;
+        private DataGridViewTextBoxColumn telefono;
+        private DataGridViewTextBoxColumn direccion;
+        private DataGridViewTextBoxColumn codigoPostal;
+        private DataGridViewTextBoxColumn fechaDeNaciomiento;
+        private DataGridViewButtonColumn modificar;
+        private DataGridViewButtonColumn eliminar;
+        private Button limpiarButton;
+        private Button buscarButton;
+        private ComboBox CondicionComboBox;
+        private TextBox condicionTextBox;
+        private Label label1;
+        private Label label2;
+        private Button nuevoClienteButton;
+        private System.ComponentModel.IContainer components;
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codigoPostal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaDeNaciomiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modificar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.limpiarButton = new System.Windows.Forms.Button();
+            this.buscarButton = new System.Windows.Forms.Button();
+            this.CondicionComboBox = new System.Windows.Forms.ComboBox();
+            this.condicionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nuevoClienteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.apellido,
+            this.dni,
+            this.mail,
+            this.telefono,
+            this.direccion,
+            this.codigoPostal,
+            this.fechaDeNaciomiento,
+            this.modificar,
+            this.eliminar});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 428);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1082, 256);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            // 
+            // dni
+            // 
+            this.dni.HeaderText = "Dni";
+            this.dni.Name = "dni";
+            // 
+            // mail
+            // 
+            this.mail.HeaderText = "Mail";
+            this.mail.Name = "mail";
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.Name = "telefono";
+            // 
+            // direccion
+            // 
+            this.direccion.HeaderText = "Direccion";
+            this.direccion.Name = "direccion";
+            // 
+            // codigoPostal
+            // 
+            this.codigoPostal.HeaderText = "Codigo Postal";
+            this.codigoPostal.Name = "codigoPostal";
+            // 
+            // fechaDeNaciomiento
+            // 
+            this.fechaDeNaciomiento.HeaderText = "Fecha de nacimiento";
+            this.fechaDeNaciomiento.Name = "fechaDeNaciomiento";
+            // 
+            // modificar
+            // 
+            this.modificar.HeaderText = "MODIFICAR";
+            this.modificar.Name = "modificar";
+            this.modificar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.modificar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // eliminar
+            // 
+            this.eliminar.HeaderText = "ELIMINAR";
+            this.eliminar.Name = "eliminar";
+            this.eliminar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.eliminar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // limpiarButton
+            // 
+            this.limpiarButton.Location = new System.Drawing.Point(321, 296);
+            this.limpiarButton.Name = "limpiarButton";
+            this.limpiarButton.Size = new System.Drawing.Size(199, 42);
+            this.limpiarButton.TabIndex = 1;
+            this.limpiarButton.Text = "Limpiar";
+            this.limpiarButton.UseVisualStyleBackColor = true;
+            // 
+            // buscarButton
+            // 
+            this.buscarButton.Location = new System.Drawing.Point(555, 296);
+            this.buscarButton.Name = "buscarButton";
+            this.buscarButton.Size = new System.Drawing.Size(199, 42);
+            this.buscarButton.TabIndex = 2;
+            this.buscarButton.Text = "Buscar";
+            this.buscarButton.UseVisualStyleBackColor = true;
+            // 
+            // CondicionComboBox
+            // 
+            this.CondicionComboBox.FormattingEnabled = true;
+            this.CondicionComboBox.Location = new System.Drawing.Point(429, 162);
+            this.CondicionComboBox.Name = "CondicionComboBox";
+            this.CondicionComboBox.Size = new System.Drawing.Size(216, 28);
+            this.CondicionComboBox.TabIndex = 3;
+            // 
+            // condicionTextBox
+            // 
+            this.condicionTextBox.Location = new System.Drawing.Point(452, 238);
+            this.condicionTextBox.Name = "condicionTextBox";
+            this.condicionTextBox.Size = new System.Drawing.Size(170, 26);
+            this.condicionTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(470, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Condicion de filtro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(486, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor de filtro";
+            // 
+            // nuevoClienteButton
+            // 
+            this.nuevoClienteButton.Location = new System.Drawing.Point(895, 12);
+            this.nuevoClienteButton.Name = "nuevoClienteButton";
+            this.nuevoClienteButton.Size = new System.Drawing.Size(199, 69);
+            this.nuevoClienteButton.TabIndex = 7;
+            this.nuevoClienteButton.Text = "NUEVO CLIENTE";
+            this.nuevoClienteButton.UseVisualStyleBackColor = true;
+            this.nuevoClienteButton.Click += new System.EventHandler(this.nuevoClienteButton_Click);
+            // 
+            // FormABMCliente
+            // 
+            this.ClientSize = new System.Drawing.Size(1106, 696);
+            this.Controls.Add(this.nuevoClienteButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.condicionTextBox);
+            this.Controls.Add(this.CondicionComboBox);
+            this.Controls.Add(this.buscarButton);
+            this.Controls.Add(this.limpiarButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormABMCliente";
+            this.Load += new System.EventHandler(this.FormABMCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void nuevoClienteButton_Click(object sender, EventArgs e)
+        {
+            Form altaClinte = new FormAltaCliente();
+            altaClinte.Show();
+
+        }
+
+        private void FormABMCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PagoAgil/Aplicacion/View/FormAltaCliente.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgil.Aplicacion.View
+{
+    class FormAltaCliente : Form
+    {
+        private TextBox nombreTextBox;
+        private TextBox apellidoTextBox;
+        private TextBox dniTextBox;
+        private TextBox mailTextBox;
+        private TextBox telefonoTextBox;
+        private TextBox direccionTextBox;
+        private TextBox codigoPostal;
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button limpiarButton;
+        private Button guardarButton;
+        private Label label8;
+
+        private void InitializeComponent()
+        {
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.apellidoTextBox = new System.Windows.Forms.TextBox();
+            this.dniTextBox = new System.Windows.Forms.TextBox();
+            this.mailTextBox = new System.Windows.Forms.TextBox();
+            this.telefonoTextBox = new System.Windows.Forms.TextBox();
+            this.direccionTextBox = new System.Windows.Forms.TextBox();
+            this.codigoPostal = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.limpiarButton = new System.Windows.Forms.Button();
+            this.guardarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(231, 54);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(201, 26);
+            this.nombreTextBox.TabIndex = 0;
+            // 
+            // apellidoTextBox
+            // 
+            this.apellidoTextBox.Location = new System.Drawing.Point(231, 107);
+            this.apellidoTextBox.Name = "apellidoTextBox";
+            this.apellidoTextBox.Size = new System.Drawing.Size(201, 26);
+            this.apellidoTextBox.TabIndex = 1;
+            // 
+            // dniTextBox
+            // 
+            this.dniTextBox.Location = new System.Drawing.Point(231, 160);
+            this.dniTextBox.Name = "dniTextBox";
+            this.dniTextBox.Size = new System.Drawing.Size(201, 26);
+            this.dniTextBox.TabIndex = 2;
+            // 
+            // mailTextBox
+            // 
+            this.mailTextBox.Location = new System.Drawing.Point(231, 213);
+            this.mailTextBox.Name = "mailTextBox";
+            this.mailTextBox.Size = new System.Drawing.Size(201, 26);
+            this.mailTextBox.TabIndex = 3;
+            // 
+            // telefonoTextBox
+            // 
+            this.telefonoTextBox.Location = new System.Drawing.Point(231, 266);
+            this.telefonoTextBox.Name = "telefonoTextBox";
+            this.telefonoTextBox.Size = new System.Drawing.Size(201, 26);
+            this.telefonoTextBox.TabIndex = 4;
+            // 
+            // direccionTextBox
+            // 
+            this.direccionTextBox.Location = new System.Drawing.Point(231, 319);
+            this.direccionTextBox.Name = "direccionTextBox";
+            this.direccionTextBox.Size = new System.Drawing.Size(201, 26);
+            this.direccionTextBox.TabIndex = 5;
+            // 
+            // codigoPostal
+            // 
+            this.codigoPostal.Location = new System.Drawing.Point(231, 372);
+            this.codigoPostal.Name = "codigoPostal";
+            this.codigoPostal.Size = new System.Drawing.Size(201, 26);
+            this.codigoPostal.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(231, 425);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(201, 26);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "DNI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(296, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Telefono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(294, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Direccion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(278, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Codigo Postal";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(253, 402);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(157, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Fecha de nacimiento";
+            // 
+            // limpiarButton
+            // 
+            this.limpiarButton.Location = new System.Drawing.Point(65, 479);
+            this.limpiarButton.Name = "limpiarButton";
+            this.limpiarButton.Size = new System.Drawing.Size(132, 55);
+            this.limpiarButton.TabIndex = 16;
+            this.limpiarButton.Text = "Limpiar";
+            this.limpiarButton.UseVisualStyleBackColor = true;
+            // 
+            // guardarButton
+            // 
+            this.guardarButton.Location = new System.Drawing.Point(448, 479);
+            this.guardarButton.Name = "guardarButton";
+            this.guardarButton.Size = new System.Drawing.Size(132, 55);
+            this.guardarButton.TabIndex = 17;
+            this.guardarButton.Text = "Guardar";
+            this.guardarButton.UseVisualStyleBackColor = true;
+            // 
+            // FormAltaCliente
+            // 
+            this.ClientSize = new System.Drawing.Size(659, 573);
+            this.Controls.Add(this.guardarButton);
+            this.Controls.Add(this.limpiarButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.codigoPostal);
+            this.Controls.Add(this.direccionTextBox);
+            this.Controls.Add(this.telefonoTextBox);
+            this.Controls.Add(this.mailTextBox);
+            this.Controls.Add(this.dniTextBox);
+            this.Controls.Add(this.apellidoTextBox);
+            this.Controls.Add(this.nombreTextBox);
+            this.Name = "FormAltaCliente";
+            this.Text = "Alta cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
