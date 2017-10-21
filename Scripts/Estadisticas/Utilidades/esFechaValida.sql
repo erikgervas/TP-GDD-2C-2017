e25fdf7e57be0b271dfb1b41a5acb83344fab866
yankkok <igfernandezsoto@gmail.com>
2017-10-21 18:26:44 -0300

    Se crea una estadistica

---
+++ Scripts/Estadisticas/Utilidades/esFechaValida.sql
@@ -0,0 +1,29 @@
+CREATE FUNCTION esFechaValida(@fecha datetime, @anio int, @trimestre int)
+RETURNS bit
+AS
+BEGIN
+
+	declare @mes int
+
+	set @mes = month(@fecha)
+
+	declare @trimesteDelAnio int
+
+		if(@mes >= 0 and @mes <= 2)
+			SET @trimesteDelAnio = 1
+
+		if(@mes >= 3 and @mes <= 5)
+			SET @trimesteDelAnio = 2
+
+		if(@mes >= 6 and @mes <= 8)
+			SET @trimesteDelAnio = 3
+		
+		if(@mes >= 9 and @mes <= 11)
+			SET @trimesteDelAnio = 4
+
+	if(year(@fecha) = @anio and @trimesteDelAnio = @trimestre)
+		return 1
+
+	return 0
+
+END
