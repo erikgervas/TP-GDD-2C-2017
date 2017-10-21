5571a4e6061adc44b4c0d3c2904f516e8f8db57d
yankkok <igfernandezsoto@gmail.com>
2017-10-21 17:04:42 -0300

    Reestructuracion

--- Scripts/Migracion/Paso 2 - Migración/ClienteParalitico.sql
+++ Scripts/Migracion/Paso 2 - Migración/ClienteParalitico.sql
@@ -1,15 +1,3 @@
-CREATE TABLE ClienteParalitico (
-	ID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	Dni NUMERIC(18,0) NOT NULL UNIQUE,
-	Nombre NVARCHAR(255) NOT NULL,
-	Apellido NVARCHAR(255) NOT NULL,
-	Nacimiento DATETIME NOT NULL,
-	Mail NVARCHAR(255) NOT NULL, 
-	Domicilio NVARCHAR(255) NOT NULL,
-	Codigo_Postal NVARCHAR(255) NOT NULL,
-	Telefono INT,
-);
-
 INSERT INTO ClienteParalitico (Dni, Nombre, Apellido, Nacimiento, Mail, Domicilio, Codigo_Postal)
 
 	SELECT DISTINCT
