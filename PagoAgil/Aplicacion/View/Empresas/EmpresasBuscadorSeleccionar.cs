38df898209a0bd01d17376c66f8013b5fe778f08
SLDG1297 <santiagoluccadeguevara@gmail.com>
2017-11-04 20:13:50 -0300

    Script para generar muchas empresas y probar

--- PagoAgil/Aplicacion/View/Empresas/EmpresasBuscadorSeleccionar.cs
+++ PagoAgil/Aplicacion/View/Empresas/EmpresasBuscadorSeleccionar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagoAgil.Aplicacion.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,23 @@ namespace PagoAgil.Aplicacion.View.Empresas
 {
     public partial class EmpresasBuscadorSeleccionar : Form
     {
-        public EmpresasBuscadorSeleccionar()
+        private EmpresasBuscador empresasBuscador;
+
+        public EmpresasBuscadorSeleccionar(EmpresasBuscador empresasBuscador)
         {
             InitializeComponent();
+            this.empresasBuscador = empresasBuscador;
+        }
+
+        private void EmpresasBuscadorSeleccionar_Load(object sender, EventArgs e)
+        {
+            this.empresaTableAdapter.Fill(this.sQL_BOYS_Data_Set.Empresa);
+
+        }
+
+        private void volverAInicioButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
